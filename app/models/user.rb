f6355d4527b35bddc183d82a48bd1a6ef50a4228
ben <bjwmedia@protonmail.com>
2018-12-03 12:15:12 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+
+
+	validates :username, presence: true
+	validates :username, uniqueness: true
+
+	has_many :tweets
+
+	mount_uploader :avatar, AvatarUploader
+
+	serialize :following, Array
+
+end
+
